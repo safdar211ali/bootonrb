605a69acc5da031cb7e815076542848b2d3a4302
ali <safdar211@hotmail.com>
2016-03-17 06:46:08 +0500

    changed index page

--- config/application.rb
+++ config/application.rb
@@ -25,9 +25,12 @@ module Bootonrb
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.action_dispatch.default_headers.merge!({
+                                                      'Access-Control-Allow-Origin' => '*',
+                                                      'Access-Control-Request-Method' => '*'
+                                                  })
 
 
 
   end
 end
-
